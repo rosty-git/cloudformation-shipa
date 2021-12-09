5add4d3cd3f069590c69d9669980bce79acce103
Rostyslav Popadyn <rostykpopadynrostykpopadyn451@gmail.com>
2021-12-09 04:54:06 +0100

    added app-deploy

---
+++ app-deploy/cmd/resource/resource.go
@@ -0,0 +1,139 @@
+package resource
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/handler"
+	"github.com/rostislavgit/cloudformation-shipa/app-deploy/internal/shipa"
+)
+
+// Create handles the Create event from the Cloudformation service.
+func Create(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	client, err := shipa.NewClient(*currentModel.ShipaHost, *currentModel.ShipaToken)
+	if err != nil {
+		return handler.ProgressEvent{}, err
+	}
+
+	_, err = client.GetApp(context.Background(), *currentModel.App)
+	// app not exists
+	if err != nil {
+		return handler.ProgressEvent{}, err
+	}
+
+	appDeploy := &shipa.AppDeploy{
+		App:      *currentModel.App,
+		Image: *currentModel.Image,
+	}
+
+	raw, _ := json.Marshal(appDeploy)
+	err = client.DeployApp(context.Background(), appDeploy)
+	if err != nil {
+		return handler.ProgressEvent{}, fmt.Errorf("ERR: %w. Payload: %s", err, string(raw))
+	}
+
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		Message:         "Create complete",
+		ResourceModel:   currentModel,
+	}, nil
+}
+
+// Read handles the Read event from the Cloudformation service.
+func Read(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	client, err := shipa.NewClient(*currentModel.ShipaHost, *currentModel.ShipaToken)
+	if err != nil {
+		return handler.ProgressEvent{}, err
+	}
+
+	deployments, err := client.ListAppDeployments(context.Background(), *currentModel.App)
+	if err != nil {
+		return handler.ProgressEvent{}, err
+	}
+
+	if len(deployments) > 0 {
+		last := deployments[len(deployments) - 1]
+		currentModel.Image = &last.Image
+	}
+
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		Message:         "Read complete",
+		ResourceModel:   currentModel,
+	}, nil
+}
+
+// Update handles the Update event from the Cloudformation service.
+func Update(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	client, err := shipa.NewClient(*currentModel.ShipaHost, *currentModel.ShipaToken)
+	if err != nil {
+		return handler.ProgressEvent{}, err
+	}
+
+	_, err = client.GetApp(context.Background(), *currentModel.App)
+	// app not exists
+	if err != nil {
+		return handler.ProgressEvent{}, err
+	}
+
+	appDeploy := &shipa.AppDeploy{
+		App:      *currentModel.App,
+		Image: *currentModel.Image,
+	}
+
+	raw, _ := json.Marshal(appDeploy)
+	err = client.DeployApp(context.Background(), appDeploy)
+	if err != nil {
+		return handler.ProgressEvent{}, fmt.Errorf("ERR: %w. Payload: %s", err, string(raw))
+	}
+
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		Message:         "Update complete",
+		ResourceModel:   currentModel,
+	}, nil
+}
+
+// Delete handles the Delete event from the Cloudformation service.
+func Delete(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	_, err := shipa.NewClient(*currentModel.ShipaHost, *currentModel.ShipaToken)
+	if err != nil {
+		return handler.ProgressEvent{}, err
+	}
+
+	//err = client.DeleteApp(context.Background(), *currentModel.App)
+	//if err != nil {
+	//	return handler.ProgressEvent{}, err
+	//}
+
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		Message:         "Delete complete",
+		ResourceModel:   currentModel,
+	}, nil
+}
+
+// List handles the List event from the Cloudformation service.
+func List(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "List complete",
+	       ResourceModel: currentModel,
+	   }
+
+	   return response, nil
+	*/
+
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: List")
+}
---
+++ app-deploy/docs/README.md
@@ -0,0 +1,80 @@
+# Shipa::AppDeploy::Item
+
+An example resource schema demonstrating some basic constructs and validation rules.
+
+## Syntax
+
+To declare this entity in your AWS CloudFormation template, use the following syntax:
+
+### JSON
+
+<pre>
+{
+    "Type" : "Shipa::AppDeploy::Item",
+    "Properties" : {
+        "<a href="#app" title="App">App</a>" : <i>String</i>,
+        "<a href="#image" title="Image">Image</a>" : <i>String</i>,
+        "<a href="#shipahost" title="ShipaHost">ShipaHost</a>" : <i>String</i>,
+        "<a href="#shipatoken" title="ShipaToken">ShipaToken</a>" : <i>String</i>
+    }
+}
+</pre>
+
+### YAML
+
+<pre>
+Type: Shipa::AppDeploy::Item
+Properties:
+    <a href="#app" title="App">App</a>: <i>String</i>
+    <a href="#image" title="Image">Image</a>: <i>String</i>
+    <a href="#shipahost" title="ShipaHost">ShipaHost</a>: <i>String</i>
+    <a href="#shipatoken" title="ShipaToken">ShipaToken</a>: <i>String</i>
+</pre>
+
+## Properties
+
+#### App
+
+Shipa application name
+
+_Required_: No
+
+_Type_: String
+
+_Update requires_: [Replacement](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-replacement)
+
+#### Image
+
+Application docker image
+
+_Required_: No
+
+_Type_: String
+
+_Update requires_: [Replacement](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-replacement)
+
+#### ShipaHost
+
+Shipa server host
+
+_Required_: Yes
+
+_Type_: String
+
+_Update requires_: [No interruption](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-no-interrupt)
+
+#### ShipaToken
+
+Shipa access token
+
+_Required_: Yes
+
+_Type_: String
+
+_Update requires_: [No interruption](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-no-interrupt)
+
+## Return Values
+
+### Ref
+
+When you pass the logical ID of this resource to the intrinsic `Ref` function, Ref returns the App.
